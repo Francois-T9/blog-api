7a647983954c549b6d7f5584afa5a83c5122f067
Francois <francois.thullier98@gmail.com>
2025-07-09 11:21:44 +0200

    Called routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const assetsPath = path.join(__dirname, "public");
 const commentsRoute = require("./routes/comments");
@@ -10,8 +11,10 @@ const { PrismaClient } = require("./generated/prisma");
 const prisma = new PrismaClient();
 // const { execSync } = require("child_process");
 // execSync("npx prisma db push", { stdio: "inherit" });
-
+app.use(bodyParser.json());
 app.use("/api/posts", postsRoute);
+app.use("/api/comments", commentsRoute);
+app.use("/api/users", usersRoute);
 app.listen(3000, () => {
   console.log("Port 3000");
 });
