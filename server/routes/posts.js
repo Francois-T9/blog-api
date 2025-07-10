76edfc83537eb44e982eae755d7435c623f25b57
Francois <francois.thullier98@gmail.com>
2025-07-10 14:31:14 +0200

    Called auth middleware to see all posts by user

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
-
+const loginController = require("../controllers/loginController");
 // get all blog posts
-router.get("/", postController.all_post_list);
+router.get("/", loginController.autheticateToken, postController.all_post_list);
 // create new post
 
 router.post("/", postController.post_create);
