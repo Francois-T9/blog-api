d90b10d2dfc3c9ca4b29e8b0b383a39894e5259a
Francois <francois.thullier98@gmail.com>
2025-07-10 14:29:29 +0200

    Added login route

--- server/app.js
+++ server/app.js
@@ -7,14 +7,18 @@ const assetsPath = path.join(__dirname, "public");
 const commentsRoute = require("./routes/comments");
 const postsRoute = require("./routes/posts");
 const usersRoute = require("./routes/users");
-const { PrismaClient } = require("./generated/prisma");
-const prisma = new PrismaClient();
+const loginRoute = require("./routes/login");
+// const { PrismaClient } = require("./generated/prisma");
+// const prisma = new PrismaClient();
 // const { execSync } = require("child_process");
 // execSync("npx prisma db push", { stdio: "inherit" });
+
 app.use(bodyParser.json());
 app.use("/api/posts", postsRoute);
 app.use("/api/comments", commentsRoute);
 app.use("/api/users", usersRoute);
-app.listen(3000, () => {
-  console.log("Port 3000");
+app.use("/api/login", loginRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Port ${process.env.PORT}`);
 });
