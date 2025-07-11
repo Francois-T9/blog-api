3fa2f127beef91d841a4e81010e38ab1bef6088d
Francois <francois.thullier98@gmail.com>
2025-07-11 15:15:28 +0200

    Added login controller

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -4,11 +4,25 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.user_login = async (req, res) => {
-  const username = req.body.username;
-  const user = { user: username };
-  jwt.sign({ user: user }, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
-    res.json({ token });
+  const { username, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: { username },
   });
+  if (!user) return res.status(401).json({ message: "User not found" });
+
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.status(401).json({ message: "Invalid credentials" });
+
+  const payload = {
+    sub: user.id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.json({ token });
 };
 
 // Authentification function
