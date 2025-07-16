7dedf3abd2952a4c2cebf9009f39250743bc5148
Francois <francois.thullier98@gmail.com>
2025-07-16 08:45:50 +0200

    called cors

--- server/app.js
+++ server/app.js
@@ -10,7 +10,9 @@ const usersRoute = require("./routes/users");
 const loginRoute = require("./routes/login");
 const passport = require("passport");
 const passportConfig = require("./config/passport");
-
+const cors = require("cors");
+app.use(cors());
+// ...existing code...
 app.use(bodyParser.json());
 app.use(passport.initialize());
 passportConfig(passport);
