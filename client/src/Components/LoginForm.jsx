14da3183e5e332ba67fb6c83328ac92a7d8bff2d
Francois <francois.thullier98@gmail.com>
2025-07-17 13:46:33 +0200

    moved login function to AuthContext

--- client/src/Components/LoginForm.jsx
+++ client/src/Components/LoginForm.jsx
@@ -1,58 +1,31 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import { useState } from "react";
+import { useAuth } from "../Context/authContext.jsx";
 import { useNavigate } from "react-router-dom";
-import { useUser } from "../Context/userContext.jsx";
-
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const { setCurrentUser, loginError, setCurrentError } = useUser();
-
+  const { login, loginError } = useAuth();
   const navigate = useNavigate();
-  const handleLogin = async (e) => {
-    e.preventDefault();
 
-    // get token from username and pwd
-    const response = await fetch("http://localhost:3000/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
-    });
-    const data = await response.json();
-    if (data.token) {
-      localStorage.setItem("token", data.token); // Store token
-      navigate("/");
-      // Optionally redirect or update UI here
-    }
-    if (data.message) {
-      setCurrentError(data.message);
-    }
-
-    // use token to get user
-    const userRequest = await fetch(
-      `http://localhost:3000/api/users/${data.userId}`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      }
-    );
-
-    if (userRequest.status == 200) {
-      setCurrentUser(await userRequest.json());
-    } else {
-      return;
-    }
+  const handleSubmit = async (e) => {
+    const success = await login(e, { username, password });
+    if (success) navigate("/");
   };
+  // const navigate = useNavigate();
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex flex-col grow">
-        <form className="p-3 flex flex-col grow " action="/" method="post">
+        <form
+          onSubmit={handleSubmit}
+          className="p-3 flex flex-col grow "
+          action="/"
+          method="post"
+        >
           <label htmlFor="username">Username</label>
           <input
             className="border border-gray-400 rounded px-2 py-1"
@@ -70,9 +43,7 @@ export default function LoginForm() {
             onChange={(e) => setPassword(e.target.value)}
           />
           {loginError ? <p className="text-red-500">{loginError}</p> : ""}
-          <button type="submit" onClick={handleLogin}>
-            Log in
-          </button>
+          <button type="submit">Log in</button>
         </form>
       </div>
       <Footer />
