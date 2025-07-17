5bd3a7b7cb9a43fff0bb5b68d64f7688983f2c7c
Francois <francois.thullier98@gmail.com>
2025-07-17 13:45:55 +0200

    added logout link is user is logged

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../Context/authContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const { user, logout } = useAuth();
+  const handleSubmit = async () => {
+    const success = await logout();
+    if (success) navigate("/");
+  };
+
   return (
     <div className="bg-gray-300 p-3 flex justify-between">
       <div className="logo flex grow">
@@ -12,12 +22,24 @@ export default function Header() {
       <div className="navbar flex grow justify-end items-center ">
         <nav>
           <ul className="flex justify-around grow gap-3">
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/signup">Signup</NavLink>
-            </li>
+            {user ? (
+              <div>
+                <li>
+                  <NavLink onClick={handleSubmit} to="/logout">
+                    Logout
+                  </NavLink>
+                </li>
+              </div>
+            ) : (
+              <div className="flex justify-around grow gap-3">
+                <li>
+                  <NavLink to="/login">Login</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/signup">Signup</NavLink>
+                </li>
+              </div>
+            )}
           </ul>
         </nav>
       </div>
