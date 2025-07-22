85085f185b946ed360ee179917e57ef255a23855
Francois <francois.thullier98@gmail.com>
2025-07-22 10:49:03 +0200

    Added getUserById function

--- client/src/Context/userContext.jsx
+++ client/src/Context/userContext.jsx
@@ -3,6 +3,7 @@ const UserContext = createContext();
 
 export function UserProvider({ children }) {
   const [allUsers, setAllUsers] = useState([]);
+  const [postAuthor, setPostAuthor] = useState();
   const getAllUsers = async () => {
     const response = await fetch("http://localhost:3000/api/users", {
       method: "GET",
@@ -14,6 +15,17 @@ export function UserProvider({ children }) {
     setAllUsers(data);
   };
 
+  const getUserById = async (id) => {
+    const response = await fetch(`http://localhost:3000/api/users/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    });
+    const data = await response.json();
+    setPostAuthor(data);
+  };
+
   const deleteUserbyId = async (id) => {
     await fetch(`http://localhost:3000/api/users/${id}`, {
       method: "DELETE",
@@ -21,12 +33,16 @@ export function UserProvider({ children }) {
         Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     });
+    // const data = await response.json();
+    setAllUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
   };
 
   const value = {
     getAllUsers,
     allUsers,
     deleteUserbyId,
+    getUserById,
+    postAuthor,
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
