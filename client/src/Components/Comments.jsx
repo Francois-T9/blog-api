c1e6e5dcf79dcd68182c7dc55523ef2609b0d794
Francois <francois.thullier98@gmail.com>
2025-07-24 13:44:50 +0200

    Created Comments component

--- client/src/Components/Comments.jsx
+++ client/src/Components/Comments.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import { useUser } from "../Context/userContext.jsx";
+import { useAuth } from "../Context/authContext.jsx";
 import { useComments } from "../Context/commentsContext.jsx";
-import { useEffect } from "react";
+function Comments({ comment }) {
+  const { allUsers } = useUser();
+  const { user } = useAuth();
+  console.log(user);
+  const { deleteComment } = useComments();
 
-function Comments() {
-  const { getAllComments, allComments } = useComments();
+  const userIdToUsername = {};
+  allUsers.forEach((user) => {
+    userIdToUsername[user.id] = user.username;
+  });
 
   return (
-    <div>
-      <p>Comments section</p>
+    <div className="comment bg-amber-100 p-2 border rounded">
+      <ul>
+        <li>{comment.content}</li>
+        <li>@{userIdToUsername[comment.authorId]}</li>
+        <li>{new Date(comment.createdAt).toLocaleString()}</li>
+      </ul>
+      {user.isAdmin ? (
+        <button
+          onClick={() => deleteComment(comment.postId, comment.id)}
+          className="bg-red-500 border p-1 cursor-pointer "
+        >
+          Delete post
+        </button>
+      ) : (
+        <p></p>
+      )}
     </div>
   );
 }
